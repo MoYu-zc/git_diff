14a52bdbffd23355a92bf2c22e076714b55a9c7e
张驰 <moyu_zc@163.com>
2022-07-10 20:51:08 +0800

    master

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+/**
+ * <p>
+ * Create at 2022/7/10 20:49
+ *
+ * @author MoYu
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("我是master");
+    }
+}
+
